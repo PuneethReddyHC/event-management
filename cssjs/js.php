c1170068edbfc7796a94044175b5a97ad73434cf
Puneeth Reddy H C <puneethreddy951@gmail.com>
2019-04-09 12:18:46 +0530

    Add files via upload

--- cssjs/js.php
+++ cssjs/js.php
@@ -15,7 +15,7 @@
   <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
   <script src="./js/google-map.js"></script>
   <script src="./js/main.js"></script>
-  <script src="./backend/action.js"></script>
+  <script src="./action.js"></script>
   
   
   <script>
